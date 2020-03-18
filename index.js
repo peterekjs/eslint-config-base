a725ea721b4e39985475ae9b1b3b6c18b500a1e4
Jiri Peterek <peterek@sourcelab.cz>
2020-03-18 10:29:55 +0100

    init rules

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const braceStyle = requrie('./lib/brace-style.js')
+
 module.exports = {
     env: {
         browser: true,
         node: true,
         es6: true
     },
+    extends: [
+        'standard'
+    ],
     globals: {
         document: 'readonly',
         navigator: 'readonly',
@@ -13,5 +18,27 @@ module.exports = {
         ecmaVersion: 2020,
         sourceType: 'module'
     },
-    plugins: ['json', 'node', 'promise']
+    plugins: [
+        'json',
+        'node',
+        'promise',
+        'standard'
+    ],
+
+    rules: {
+        'brace-style': braceStyle,
+        'arrow-parens': [2, 'as-needed'],
+        'comma-dangle': [2, 'only-multiline'],
+        'curly': [2, 'multi-line'],
+        'new-parens': [2, 'never'],
+        'no-cond-assign': [2, 'except-parens'],
+        'no-console': [1, { allow: ['info', 'warn', 'error'] }],
+        'no-constant-condition': [2, { checkLoops: true }],
+        'no-dupe-else-if': 2,
+        'no-empty': 0,
+        'no-multiple-empty-lines': [2, { max: 2, maxEOF: 0 }],
+        'no-undefined': 2,
+        'no-void': 0,
+        'quote-props': [2, 'consistent-as-needed'],
+    }
 }
